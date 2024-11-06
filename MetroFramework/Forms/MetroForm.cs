0bf957d8f4c5868a356c431ed94b7054f280c236
zz <zz@test.com>
2024-11-06 16:20:32 +0800

    debug windows button ok to use the default button

--- MetroFramework/Forms/MetroForm.cs
+++ MetroFramework/Forms/MetroForm.cs
@@ -591,7 +591,7 @@ namespace MetroFramework.Forms
                 case (int)WinApi.Messages.WM_SIZE:
                     if (windowButtonList != null)
                     {
-                        MetroFormButton btn;
+                        Button btn;
                         windowButtonList.TryGetValue(WindowButtons.Maximize, out btn);
                         if (btn == null) return;
                         if (WindowState == FormWindowState.Normal)
@@ -699,17 +699,17 @@ namespace MetroFramework.Forms
             Close
         }
 
-        private Dictionary<WindowButtons, MetroFormButton> windowButtonList;
+        private Dictionary<WindowButtons, Button> windowButtonList;
 
         private void AddWindowButton(WindowButtons button)
         {
             if (windowButtonList == null)
-                windowButtonList = new Dictionary<WindowButtons, MetroFormButton>();
+                windowButtonList = new Dictionary<WindowButtons, Button>();
 
             if (windowButtonList.ContainsKey(button))
                 return;
 
-            MetroFormButton newButton = new MetroFormButton();
+            Button newButton = new Button();
 
             if (button == WindowButtons.Close)
             {
@@ -727,8 +727,8 @@ namespace MetroFramework.Forms
                     newButton.Text = "2";
             }
 
-            newButton.Style = Style;
-            newButton.Theme = Theme;
+            //newButton.Style = Style;
+            //newButton.Theme = Theme;
             newButton.Tag = button;
             newButton.Size = new Size(25, 20);
             newButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
@@ -741,7 +741,7 @@ namespace MetroFramework.Forms
 
         private void WindowButton_Click(object sender, EventArgs e)
         {
-            var btn = sender as MetroFormButton;
+            var btn = sender as Button;
             if (btn != null)
             {
                 var btnFlag = (WindowButtons)btn.Tag;
@@ -778,11 +778,11 @@ namespace MetroFramework.Forms
             Point firstButtonLocation = new Point(ClientRectangle.Width - borderWidth - 25, borderWidth);
             int lastDrawedButtonPosition = firstButtonLocation.X - 25;
 
-            MetroFormButton firstButton = null;
+            Button firstButton = null;
 
             if (windowButtonList.Count == 1)
             {
-                foreach (KeyValuePair<WindowButtons, MetroFormButton> button in windowButtonList)
+                foreach (KeyValuePair<WindowButtons, Button> button in windowButtonList)
                 {
                     button.Value.Location = firstButtonLocation;
                 }
